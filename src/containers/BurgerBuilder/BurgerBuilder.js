dd5714f1a7bc9bf5ec29eaee4fcc741eba833dcd
crazy-me-dev <daniel124eng@gmail.com>
2021-03-22 13:20:53 -0700

    build controls added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import BuildControl from '../../components/Burger/BuildControls/BuildControl/BuildControl';
 
 export default class BurgerBuilder extends Component {
   state = {
@@ -16,7 +18,7 @@ export default class BurgerBuilder extends Component {
     return (
       <Auxiliary>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Auxiliary>
     );
   }
