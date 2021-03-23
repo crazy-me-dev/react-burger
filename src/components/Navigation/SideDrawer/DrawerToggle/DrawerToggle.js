d875e045e87fa5b02ee115b91fea157321cc56a2
crazy-me-dev <daniel124eng@gmail.com>
2021-03-23 16:53:19 -0700

    hamburger menu

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import classes from './DrawerToggle.module.css';
 
-const DrawerToggle = props => <div onClick={props.clicked}>MENU</div>;
+const DrawerToggle = props => (
+  <div onClick={props.clicked} className={classes.DrawerToggle}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 
 export default DrawerToggle;
