a352dcf1c5fd5bfec55bc8db3eafc23905c0a35f
crazy-me-dev <daniel124eng@gmail.com>
2021-03-30 12:11:16 -0700

    contact data form

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
@@ -38,6 +39,10 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           CheckoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.url + '/contact-data'}
+          component={ContactData}
         />
       </div>
     );
