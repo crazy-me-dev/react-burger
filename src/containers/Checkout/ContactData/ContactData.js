c9e41308e9826937c0fcc01b8d02f9096f476959
crazy-me-dev <daniel124eng@gmail.com>
2021-03-30 12:44:06 -0700

    spinner and push on order checkout

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import classes from './ContactData.module.css';
 import Button from '../../../components/UI/Button/Button';
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
   state = {
@@ -12,58 +14,98 @@ class ContactData extends Component {
       city: '',
       state: '',
       zipcode: ''
-    }
+    },
+    loading: false
+  };
+
+  orderHandler = event => {
+    // stops the page from reloading
+    event.preventDefault();
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      // price would be checked on server to stop manipulation
+      price: this.props.price,
+      customer: {
+        name: this.state.name,
+        address: {
+          street: 'jlkj',
+          city: 'Nowhere',
+          zipCode: '98777'
+        },
+        email: 'test@test.com',
+        phone: 555555555
+      },
+      deliveryMethod: 'UberEats'
+    };
+    axios
+      .post('/orders.json', order)
+      .then(response => {
+        this.setState({ loading: false });
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
   };
 
   render() {
+    let form = (
+      <form>
+        <input
+          className={classes.Input}
+          type='text'
+          name='name'
+          placeholder='Your Name'
+        />
+        <input
+          className={classes.Input}
+          type='email'
+          name='email'
+          placeholder='Your Email'
+        />
+        <input
+          className={classes.Input}
+          type='text'
+          name='phone'
+          placeholder='Phone Number'
+        />
+        <input
+          className={classes.Input}
+          type='text'
+          name='street'
+          placeholder='Address'
+        />
+        <input
+          className={classes.Input}
+          type='text'
+          name='city'
+          placeholder='City'
+        />
+        <input
+          className={classes.Input}
+          type='text'
+          name='state'
+          placeholder='State'
+        />
+        <input
+          className={classes.Input}
+          type='text'
+          name='zipcode'
+          placeholder='Zip Code'
+        />
+        <Button btnType='Success' clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Information</h4>
-        <form>
-          <input
-            className={classes.Input}
-            type='text'
-            name='name'
-            placeholder='Your Name'
-          />
-          <input
-            className={classes.Input}
-            type='email'
-            name='email'
-            placeholder='Your Email'
-          />
-          <input
-            className={classes.Input}
-            type='text'
-            name='phone'
-            placeholder='Phone Number'
-          />
-          <input
-            className={classes.Input}
-            type='text'
-            name='street'
-            placeholder='Address'
-          />
-          <input
-            className={classes.Input}
-            type='text'
-            name='city'
-            placeholder='City'
-          />
-          <input
-            className={classes.Input}
-            type='text'
-            name='state'
-            placeholder='State'
-          />
-          <input
-            className={classes.Input}
-            type='text'
-            name='zipcode'
-            placeholder='Zip Code'
-          />
-          <Button btnType='Success'>ORDER</Button>
-        </form>
+        {form}
       </div>
     );
   }
