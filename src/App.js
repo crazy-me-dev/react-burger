2b201234800715b323802dfc95b8ed076d841fba
crazy-me-dev <daniel124eng@gmail.com>
2021-04-19 12:49:20 -0700

    authentication page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Orders from './containers/Orders/Orders';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
       <Layout>
         <Route path='/checkout' component={Checkout} />
         <Route path='/orders' component={Orders} />
+        <Route path='/auth' component={Auth} />
         <Route path='/' exact component={BurgerBuilder} />
       </Layout>
     );
