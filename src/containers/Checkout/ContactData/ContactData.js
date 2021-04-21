ab6230c5b7f97c65f817ef50220fd7f799771cc3
crazy-me-dev <daniel124eng@gmail.com>
2021-04-21 16:52:30 -0700

    index userId from firebase for orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -112,7 +112,8 @@ class ContactData extends Component {
       ingredients: this.props.ings,
       // price would be checked on server to stop manipulation
       price: this.props.price,
-      orderData: formData
+      orderData: formData,
+      userId: this.props.userId
     };
     this.props.onOrderBurger(order, this.props.token);
   };
@@ -214,7 +215,8 @@ const mapStateToProps = state => {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   };
 };
 
